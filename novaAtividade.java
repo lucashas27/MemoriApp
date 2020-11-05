9017636581d986860636890b716f4fac196dc43f
Lucas Alves <49699847+lucashas27@users.noreply.github.com>
2020-11-05 19:11:15 -0300

    Add files via upload

---
+++ novaAtividade.java
@@ -0,0 +1,20 @@
+package AgendaInteligente;
+
+public class novaAtividade {
+
+    public String classificacaoDeAtividades;
+
+    void nomearAtividade() {
+
+    }
+
+
+    public String getClassificacaoDeAtividades() {
+        return classificacaoDeAtividades;
+    }
+
+    public void setClassificacaoDeAtividades(String classificacaoDeAtividades) {
+        this.classificacaoDeAtividades = classificacaoDeAtividades;
+    }
+
+}
